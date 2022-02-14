f98b064b26302aad59cbe46d5bbf819276e7ad77
brigardner <97481836+brigardner@users.noreply.github.com>
2022-02-14 17:55:31 -0500

    Got very first basic user POST method to work

---
+++ src/main/java/com/revature/team4/beans/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.revature.team4.beans.controllers;
+
+import com.revature.team4.beans.entities.User;
+import com.revature.team4.beans.repositories.ListingRepo;
+import com.revature.team4.beans.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    public final UserRepo userRepo;
+    public final ListingRepo listingRepo;
+
+    @Autowired
+    public UserController(UserRepo userRepo, ListingRepo listingRepo) {
+        this.userRepo = userRepo;
+        this.listingRepo = listingRepo;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void newUser(@RequestBody User user) { userRepo.save(user); }
+}
