63868af22c33f61baf2b81a1e4395cb85974ae1d
Donald Farrar <donfar09@gmail.com>
2022-02-16 13:03:25 -0800

    finished the locations package

--- src/main/java/com/revature/team4/beans/apiResponseDAO/locations/LocationEntityGroupDAO.java
+++ src/main/java/com/revature/team4/beans/apiResponseDAO/locations/LocationEntityGroupDAO.java
@@ -1,4 +1,33 @@
 package com.revature.team4.beans.apiResponseDAO.locations;
 
+import java.util.List;
+
 public class LocationEntityGroupDAO {
+
+    public String group;
+    public List<LocationEntityDAO> entities;
+
+    public LocationEntityGroupDAO() {
+    }
+
+    public LocationEntityGroupDAO(String group, List<LocationEntityDAO> entities) {
+        this.group = group;
+        this.entities = entities;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public List<LocationEntityDAO> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<LocationEntityDAO> entities) {
+        this.entities = entities;
+    }
 }
