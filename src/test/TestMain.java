33be8d42de45d19fa87de102a3b2bdb94faa722f
brigardner <97481836+brigardner@users.noreply.github.com>
2022-02-17 15:53:18 -0500

    Removed second main class

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.team4.beans.apiResponseDAO.propertiesList.PropertyListDAO;
+import com.revature.team4.beans.controllers.FindListingsController;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -32,6 +33,12 @@ public class TestMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println("TestMain");
+
+
+        FindListingsController fl = new FindListingsController();
+        System.out.println(fl.findListings());
 
     }
 }
